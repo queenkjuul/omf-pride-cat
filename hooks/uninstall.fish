49bb367ffc9b045910490f86ad35b1e5a5d08422
queenkjuul <julie.hill@pm.me>
2020-02-28 23:43:07 +0000

    fix install and uninstall scripts

---
+++ hooks/uninstall.fish
@@ -0,0 +1,10 @@
+function pride_reset_defaults
+    set -e __pride_flag default 
+    set -e __pride_bind_mode default
+    set -e __pride_commie default
+    set -e __pride_hostname default
+    set -e __pride_username default
+    set -e __pride_cat default
+    set -e __pride_cat_status on
+    echo "pride cat theme uninstalled"
+end
