7ba29f4ad0aeeca71566737532d7c767985a150e
queenkjuul <julie.hill@pm.me>
2020-02-28 09:46:03 -0600

    readme, gay flag

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Slavic Cat
+# [Slavic] Pride Cat
 
 A fish (shell) theme with a Slavic cat and a rainbow that signifies git status.
 
+This is slightly modified from the original Slavic Cat, with a pride flag prompt. All of the colors in the scheme have been pulled from various queer pride flags. I am planning to eventually provide additional flag prompts. 
 
 [![Oh My Fish](https://img.shields.io/badge/Framework-Oh_My_Fish-blue.svg?style=flat)](https://github.com/oh-my-fish/oh-my-fish)
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)](/LICENSE)
@@ -19,12 +20,32 @@ shell for OS X, Linux, and the rest of the family
 Once Oh My Fish is installed:
 
     omf install https://github.com/queenkjuul/omf-pride-cat
+
+### Options
+
+Choose Flag:
+
+    set -U __pride_flag gay            # default is trans. current options are just gay and trans
+
+Dirty Commie
+
+    set -U __pride_commie off          # turns off the hammer and sickle icon. default is on
+
+Username
+
+    set -U __pride_username off        # default is on
+
+fish bind mode
+
+    set -U __pride_bind_mode off       # default is on
+
+
     
 [forked from Slavic Cat](https://github.com/yangwao/omf-theme-slavic-cat) which was in turn [forked from Toaster](https://github.com/oh-my-fish/theme-toaster)
 
 ## Features
 
- * Minimal Look & Prompt
+ * gaaaaayyyyyy
  * Current short version pwd
  * Display branch for Git projects
  * Git project dirty status indicator *meow* **(R,C,A,U,M,?)** *meow*
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,25 +1,28 @@
 # original slavic-cat forked from Toaster https://github.com/oh-my-fish/theme-toaster
 # this was forked from slavic-cat https://github.com/yangwao/omf-theme-slavic-cat
 
-set __slavic_color_orange FFA52C
-set __slavic_color_blue 55CDFC
-set __slavic_color_green A6E22E
-set __slavic_color_yellow FFF430
-set __slavic_color_pink F7A8B8
-set __slavic_color_grey 554F48
-set __slavic_color_white F1F1F1
-set __slavic_color_purple 9C59D1
-set __slavic_color_lilac B77FDD
-set __slavic_color_red CD0000
-
-function __slavic_color_echo
+set __pride_color_orange FFA52C
+set __pride_color_blue 55CDFC
+set __pride_color_green A6E22E
+set __pride_color_yellow FFF430
+set __pride_color_pink F7A8B8
+set __pride_color_grey 554F48
+set __pride_color_white F1F1F1
+set __pride_color_purple 9C59D1
+set __pride_color_lilac B77FDD
+set __pride_color_red CD0000
+set __pride_color_bright_pink f92673
+if test -z "$__pride_flag"
+  set -U __pride_flag trans
+end
+function __pride_color_echo
   set_color $argv[1]
   if test (count $argv) -eq 2
     echo -n $argv[2]
   end
 end
 
-function __slavic_current_folder
+function __pride_current_folder
   if test $PWD = '/'
     echo -n '/'
   else
@@ -27,75 +30,92 @@ function __slavic_current_folder
   end
 end
 
-function __slavic_git_status_codes
+function __pride_git_status_codes
   echo (git status --porcelain ^/dev/null | sed -E 's/(^.{3}).*/\1/' | tr -d ' \n')
 end
 
-function __slavic_git_branch_name
+function __pride_git_branch_name
   echo (git rev-parse --abbrev-ref HEAD ^/dev/null)
 end
 
-function __slavic_rainbow
+function __pride_rainbow
   if echo $argv[1] | grep -q -e $argv[3]
-    __slavic_color_echo $argv[2] "彡ミ"
+    __pride_color_echo $argv[2] "彡ミ"
   end
 end
 
-function __slavic_git_status_icons
-  set -l git_status (__slavic_git_status_codes)
+function __pride_git_status_icons
+  set -l git_status (__pride_git_status_codes)
 
-  __slavic_rainbow $git_status $__slavic_color_pink 'D'
-  __slavic_rainbow $git_status $__slavic_color_orange 'R'
-  __slavic_rainbow $git_status $__slavic_color_white 'C'
-  __slavic_rainbow $git_status $__slavic_color_green 'A'
-  __slavic_rainbow $git_status $__slavic_color_blue 'U'
-  __slavic_rainbow $git_status $__slavic_color_lilac 'M'
-  __slavic_rainbow $git_status $__slavic_color_grey '?'
+  __pride_rainbow $git_status $__pride_color_pink 'D'
+  __pride_rainbow $git_status $__pride_color_orange 'R'
+  __pride_rainbow $git_status $__pride_color_white 'C'
+  __pride_rainbow $git_status $__pride_color_green 'A'
+  __pride_rainbow $git_status $__pride_color_blue 'U'
+  __pride_rainbow $git_status $__pride_color_lilac 'M'
+  __pride_rainbow $git_status $__pride_color_grey '?'
 end
 
-function __slavic_git_status
+function __pride_git_status
   # In git
-  if test -n (__slavic_git_branch_name)
+  if test -n (__pride_git_branch_name)
 
-    #__slavic_color_echo $__slavic_color_red " ☭ "
-    __slavic_color_echo $__slavic_color_white (__slavic_git_branch_name)
+    #__pride_color_echo $__pride_color_red " ☭ "
+    __pride_color_echo $__pride_color_white (__pride_git_branch_name)
 
-    if test -n (__slavic_git_status_codes)
-      __slavic_color_echo $__slavic_color_pink ' ●'
-      __slavic_color_echo $__slavic_color_white ' (^._.^)ﾉ'
-      __slavic_git_status_icons
+    if test -n (__pride_git_status_codes)
+      __pride_color_echo $__pride_color_pink ' ●'
+      # __pride_color_echo $__pride_color_white ' (^._.^)ﾉ'
+      __pride_color_echo $__pride_color_white '(=^･^=)ﾉ'
+      __pride_git_status_icons
     else
-      __slavic_color_echo $__slavic_color_green ' ○'
+      __pride_color_echo $__pride_color_green ' ○'
     end
   end
 end
 
 function fish_prompt
-  set_color $__slavic_color_lilac
-  printf '['
-  switch $fish_bind_mode
-    case default
-      set_color red
-      printf 'n'
-    case insert
-      set_color green
-      printf 'i'
-    case visual
-      set_color magenta
-      printf 'v'
+  set_color $__pride_color_lilac
+  if test "$__pride_bind_mode" != "off"
+    printf '['
+    switch $fish_bind_mode
+      case default
+        set_color red
+        printf 'n'
+      case insert
+        set_color green
+        printf 'i'
+      case visual
+        set_color magenta
+        printf 'v'
+    end
+    set_color $__pride_color_lilac
+    printf '] '
   end
-  set_color $__slavic_color_lilac
-  printf '] '
-  __slavic_color_echo $__slavic_color_purple (whoami)
-  __slavic_color_echo $__slavic_color_blue ":"
-  __slavic_color_echo $__slavic_color_lilac (prompt_pwd)
-  __slavic_color_echo $__slavic_color_red " ☭ "
-  __slavic_git_status
+  if test "$__pride_username" != "off"
+    __pride_color_echo $__pride_color_bright_pink (whoami)
+    __pride_color_echo $__pride_color_blue ":"
+  end
+  __pride_color_echo $__pride_color_lilac (prompt_pwd)
+  if test "$__pride_commie" != "off"
+    __pride_color_echo $__pride_color_red " ☭ "
+  end
+  __pride_git_status
 
   echo
-  __slavic_color_echo $__slavic_color_blue "➤"
-  __slavic_color_echo $__slavic_color_pink "➤"
-  __slavic_color_echo $__slavic_color_white "➤"
-  __slavic_color_echo $__slavic_color_pink "➤"
-  __slavic_color_echo $__slavic_color_blue "➤  "
+  switch $__pride_flag
+    case trans
+      __pride_color_echo $__pride_color_blue "➤"
+      __pride_color_echo $__pride_color_pink "➤"
+      __pride_color_echo $__pride_color_white "➤"
+      __pride_color_echo $__pride_color_pink "➤"
+      __pride_color_echo $__pride_color_blue "➤  "
+    case gay
+      __pride_color_echo $__pride_color_red "➤"
+      __pride_color_echo $__pride_color_orange "➤"
+      __pride_color_echo $__pride_color_yellow "➤"
+      __pride_color_echo $__pride_color_green "➤"
+      __pride_color_echo $__pride_color_blue "➤"
+      __pride_color_echo $__pride_color_purple "➤  "
+  end
 end
