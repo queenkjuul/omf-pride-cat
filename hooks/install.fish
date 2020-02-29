f0f783003ce13859500a4c3abc5119c2a9dae9d2
queenkjuul <julie.hill@pm.me>
2020-02-29 00:21:34 +0000

    fix install and uninstall scripts

--- hooks/install.fish
+++ hooks/install.fish
@@ -1,2 +1,9 @@
-fish $path/functions/pride_reset_defaults
+set -U __pride_flag default 
+set -U __pride_bind_mode off
+set -U __pride_commie default
+set -U __pride_hostname default
+set -U __pride_username default
+set -U __pride_cat default
+set -U __pride_cat_status on
+echo "pride cat theme reset to defaults"
 
